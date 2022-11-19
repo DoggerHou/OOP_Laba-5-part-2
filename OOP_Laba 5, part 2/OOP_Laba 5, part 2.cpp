c8b828a2329dc9842ae32f47a8de58eec32e18bd
Artem <Artem@DESKTOP-5RBGIAD>
2022-11-19 21:52:06 +0500

    Добавьте файлы проекта.

---
+++ OOP_Laba 5, part 2/OOP_Laba 5, part 2.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Animal {
+public:
+	Animal() {
+		cout << "Animal()\n";
+	}
+	virtual ~Animal() {
+		cout << "~Animal()\n";
+	}
+	virtual void sound() {
+		cout << "Animal sound\n";
+	}
+};
+
+class Dog :public Animal {
+	Dog() {
+		cout << "Dog()\n";
+	}
+	~Dog() {
+		cout << "~Dog()\n";
+	}
+	void sound() override {
+		cout << "Dod sound\n";
+	}
+};
+
+class Cat :public Animal {
+	Cat() {
+		cout << "Cat()\n";
+	}
+	~Cat() {
+		cout << "~Cat()\n";
+	}
+	void sound() override {
+		cout << "Cat sound\n";
+	}
+};
+
+int main()
+{
+
+}
